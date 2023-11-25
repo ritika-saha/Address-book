be11589b5b618831b16ba734582b31432235467f
ritika-saha <ritika882001@gmail.com>
2023-11-26 04:05:23 +0530

    minor changes

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,45 +1,55 @@
 import java.util.Scanner;
-public class AddressBookMain {
 
-    public static void main(String[] args){
+public class AddressBookMain {
+    public static void main(String[] args) {
         System.out.println("----------------Welcome to Address Book Program--------------");
 
-        //createContact branch to create contact
-        Scanner sc=new Scanner(System.in);
-        //addContacts branch to add
-        contact c1=new contact("ritika", "saha", "fgrg", "fdrfg", "frgerg", "32423", "234324324", "dfbhsfd");
-        AddressBook ab1=new AddressBook();
-        ab1.addAddress(c1);
-        int choice=-1;
-        System.out.println("Enter 1 to add contact");
-        System.out.println("enter 0 to exit");
-        choice=sc.nextInt();
-        sc.nextLine();
-        while(choice!=0){
-            if(choice==1){
-                 System.out.println("enter first and last names, address, city, state, zip, phone number and email...");
-                 String fname=sc.nextLine();
-                 String lname=sc.nextLine();
-                 String address=sc.nextLine();
-                 String city=sc.nextLine();
-                 String zip=sc.nextLine();
-                 String state=sc.nextLine();
-                 String phone=sc.nextLine();
-                 String email=sc.nextLine();
-                 contact c=new contact(fname, lname, address, city, state, zip, phone, email);
-                 ab1.addAddress(c);
+        Scanner sc = new Scanner(System.in);
+        AddressBook ab1 = new AddressBook();
+        int choice = -1;
 
-                 System.out.println("Enter 1 to add contact");
-        System.out.println("enter 0 to exit");
-        choice=sc.nextInt();
-        sc.nextLine();
+        while (choice != 0) {
+            System.out.println("Enter 1 to add contact");
+            System.out.println("Enter 2 to edit contact");
+            System.out.println("Enter 0 to exit");
 
+            try {
+                choice = sc.nextInt();
+                sc.nextLine(); // Consume the newline character
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter first and last names, address, city, state, zip, phone number, and email...");
+                        String fname = sc.nextLine();
+                        String lname = sc.nextLine();
+                        String address = sc.nextLine();
+                        String city = sc.nextLine();
+                        String zip = sc.nextLine();
+                        String state = sc.nextLine();
+                        String phone = sc.nextLine();
+                        String email = sc.nextLine();
+                        contact c = new contact(fname, lname, address, city, state, zip, phone, email);
+                        ab1.addAddress(c);
+                        break;
+
+                    case 2:
+                        System.out.println("Enter the firstname and lastname of person you want to edit");
+                        String fn = sc.nextLine();
+                        String ln = sc.nextLine();
+                        ab1.editAddress(fn, ln);
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice. Please enter a valid option.");
+                }
+
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                sc.nextLine(); // Consume the invalid input
             }
-
-            else if(choice==0)
-            break;
         }
-        
-        
     }
 }
