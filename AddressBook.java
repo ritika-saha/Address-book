fe8919e85be0e506a62d1ad5ad29eac115913794
ritika-saha <ritika882001@gmail.com>
2023-11-26 03:26:29 +0530

    seperating contact class

--- AddressBook.java
+++ AddressBook.java
@@ -1,70 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class contact {
-    private String fname;
-    private String lname;
-    private String address;
-    private String city;
-    private String zip;
-    private String state;
-    private String phone;
-    private String email;
-
-    public contact(String fname, String lname, String address, String city, String state, String zip, String phone,
-            String email) {
-        this.fname = fname;
-        this.lname = lname;
-        this.address = address;
-        this.city = city;
-        this.zip = zip;
-        this.state = state;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public String getLname() {
-        return lname;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void updateContact(String address, String city, String state, String zip, String phone, String email) {
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email;
-    }
-
-}
 
 
 
@@ -91,7 +27,7 @@ class AddressBook {
                  String state=contactList.get(i).getState();
                  String phone=contactList.get(i).getPhone();
                  String email=contactList.get(i).getEmail();
-                System.out.printf( "%s %s  \t\t\t %s  \t\t%s  \t\t %s \t\t %s  \t\t %s  \t\t %s\n",
+                System.out.printf( "%s %s  \t\t %s  \t\t%s  \t\t\t %s \t\t %s  \t\t %s  \t %s\n",
                 fname, lname, address, city, state, zip, phone, email);
             }
         }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,5 +2,14 @@ public class AddressBookMain {
 
     public static void main(String[] args){
         System.out.println("----------------Welcome to Address Book Program--------------");
+
+        //createContact branch to create contact
+
+        //addContact branch to add
+        contact c1=new contact("ritika", "saha", "fgrg", "fdrfg", "frgerg", "32423", "234324324", "dfbhsfd");
+        AddressBook ab1=new AddressBook();
+        ab1.addAddress(c1);
+        
+        
     }
 }
